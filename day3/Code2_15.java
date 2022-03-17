841b23f2f8bce5e9c55905e3f9630e1732201c73
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-17 13:40:48 +0900

    キーボード入力

--- day3/Code2_15.java
+++ day3/Code2_15.java
@@ -1,9 +1,9 @@
 import java.util.*;
 public class Code2_15{
   public static void main(String[] args){
-		System.out.println("Name>>");
+		System.out.print("Name>>");
 		String name = new Scanner(System.in).nextLine();
-		System.out.println("Age>>");
+		System.out.print("Age>>");
 		int age = new Scanner(System.in).nextInt();
 		System.out.println("ようこそ"+age+"歳の"+name+"さん");
   }
