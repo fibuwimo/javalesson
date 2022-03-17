bea8610df0eabf72c1f77e43279876f1e0f9351f
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-17 13:25:44 +0900

    キーボード入力

--- day3/Code2_14.java
+++ day3/Code2_14.java
@@ -1,6 +1,8 @@
+import java.util.*;
 public class Code2_14{
   public static void main(String[] args){
 		int r =new java.util.Random().nextInt(90);
+		//nextInt(6)+5
 		System.out.println("あなたはたぶん、"+r+"歳ですね");
   }
 }
