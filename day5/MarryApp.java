dce9e34e285ffb3c258494286866d36a38bd9852
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-23 12:54:37 +0900

    MarryApp追加

--- day5/MarryApp.java
+++ day5/MarryApp.java
@@ -5,11 +5,12 @@ public class MarryApp{
 		 *女性16歳
 		 *男性18歳
 		 * */
+		/*DRY(Don't repeat yourself)*/
 		System.out.print("性別(男性,女性)>>");
 		String gender=new Scanner(System.in).next();
 		System.out.print("年齢>>");
 		int age=new Scanner(System.in).nextInt();
-		if(gender.equals("女性")){
+		/*if(gender.equals("女性")){
 			if(age>=16){
 			System.out.print(age+"歳で"+gender+"のあなたは結婚できます");
 			}else{
@@ -21,6 +22,13 @@ public class MarryApp{
 			}else{
 			System.out.print(age+"歳で"+gender+"のあなたは結婚できません");
 			}
+		}
+	}
+		*/
+		if((gender.contains("女") && age>=16) || (gender.contains("男") && age>=18)){
+			System.out.print(age+"歳で"+gender+"のあなたは結婚できます");
+		}else{
+			System.out.print(age+"歳で"+gender+"のあなたは結婚できません");
 		}
 	}
 }
