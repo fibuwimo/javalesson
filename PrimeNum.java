6c1bf34a357fb349fcdf7956947be6a8f2eb7df7
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-24 19:06:19 +0900

    PrimeNum追加

--- PrimeNum.java
+++ PrimeNum.java
@@ -3,12 +3,12 @@ public class PrimeNum{
 	public static void main(String[] args){
 		System.out.print("自然数を入力>>");
 		int n = new Scanner(System.in).nextInt();
-		int m = 2;
+		int m = 3;
 		String ans = "である";
 		if(n<1){
 			System.out.print("自然数ではない");
 		}else{
-			if(n==2){
+			if(n==2||n==3){
 				ans = "である";
 			}else if(n==1||n%2==0){
 				ans = "ではない";
@@ -18,7 +18,7 @@ public class PrimeNum{
 						ans = "ではない";
 					}else{
 					}
-					m++;
+					m = m+2;
 				}while(m < n/2);
 			}
 			System.out.print("素数"+ans);
