61aa19a3da4cdba51610fdbcf0c3171690e566bb
fibuwimo <ray-rem-craim-requiem@ezweb.ne.jp>
2022-03-29 14:42:59 +0900

    day9追加

---
+++ Test9.java
@@ -0,0 +1,8 @@
+import java.util.*;
+public class AkitaMain{
+	public static void main(String[] args){
+		System.out.print("人数を入力>>");
+		int n = new Scanner(System.in).nextInt();
+		for(int i=0;i<n;)
+	}
+}
